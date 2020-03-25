420339c0449800ae5c977582262f23f9d01be2f7
Leigh Halliday <leighhalliday@gmail.com>
2020-03-25 14:17:44 -0400

    Demo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import keplerGlReducer from "kepler.gl/reducers";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { taskMiddleware } from "react-palm/tasks";
+import { Provider, useDispatch } from "react-redux";
+import KeplerGl from "kepler.gl";
+import { addDataToMap } from "kepler.gl/actions";
+import useSwr from "swr";
 
-function App() {
+const reducers = combineReducers({
+  keplerGl: keplerGlReducer
+});
+
+const store = createStore(reducers, {}, applyMiddleware(taskMiddleware));
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Map />
+    </Provider>
   );
 }
 
-export default App;
+function Map() {
+  const dispatch = useDispatch();
+  const { data } = useSwr("covid", async () => {
+    const response = await fetch(
+      "https://gist.githubusercontent.com/leighhalliday/a994915d8050e90d413515e97babd3b3/raw/a3eaaadcc784168e3845a98931780bd60afb362f/covid19.json"
+    );
+    const data = await response.json();
+    return data;
+  });
+
+  React.useEffect(() => {
+    if (data) {
+      dispatch(
+        addDataToMap({
+          datasets: {
+            info: {
+              label: "COVID-19",
+              id: "covid19"
+            },
+            data
+          },
+          option: {
+            centerMap: true,
+            readOnly: false
+          },
+          config: {}
+        })
+      );
+    }
+  }, [dispatch, data]);
+
+  return (
+    <KeplerGl
+      id="covid"
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_API}
+      width={window.innerWidth}
+      height={window.innerHeight}
+    />
+  );
+}
